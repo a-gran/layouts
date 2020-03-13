6bea29efc2d3170f4e9d832bbeee9fd300afdb46
alex <awebdeff@gmail.com>
2020-03-13 20:04:06 +0300

    добавлен блок новостей

--- 0-1/free-psd/js/main.js
+++ 0-1/free-psd/js/main.js
@@ -1,14 +1,11 @@
 $(function() {
 
-  $('.slider__inner').slick({
+  $('.slider__inner, .news__slider-inner').slick({
     nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
     infinite: false
   });
 
   $('select').styler();
-
-
-
 
 });
